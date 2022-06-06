73a5178cb4d21a856770e9f5251697706a411967
SUMIT SAURABH <sumitsaurabh799@gmail.com>
2022-06-06 09:57:53 +0530

    Add catalog item

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shopping_app/pages/home_page.dart';
 import 'package:shopping_app/pages/login_page.dart';
 import 'package:shopping_app/utils/routes.dart';
+import 'package:shopping_app/utils/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,9 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      // home: HomePage(),
-      //  initialRoute:"/home",
       routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,57 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:shopping_app/models/catalog.dart';
+import 'package:shopping_app/utils/item_widget.dart';
+import 'dart:convert';
 import '../utils/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Catalog App"),
-      ),
-      body: Center(
-        child: Container(
-          child: Text(
-            "Welcome to login Page",
-            style: TextStyle(color: Colors.blue),
-          ),
+        title: Text(
+          "Catalog App",
         ),
       ),
+      body: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+          ? ListView.builder(
+              itemCount: CatalogModel.items.length,
+              itemBuilder: (context, index) {
+                return ItemWidget(
+                  item: CatalogModel.items[index],
+                  key: Key("image"),
+                );
+              },
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: MyDrawer(),
     );
   }
